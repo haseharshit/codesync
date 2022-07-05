209d1e358fe83c2c377bcdb90f5dfcb02fde6d7d
haseharshit <haseharshit@gmail.com>
2022-07-05 12:29:33 +0530

    Socket Functionality Implemented

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,42 @@
 require('dotenv').config();
 const express= require("express");
 const router= require("./routes");
+const {Server} = require('socket.io')
 const app=express();
 const cors =require("cors");
+const http=require('http');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
 app.use(router);
+const server =http.createServer(app);
+const io= new Server(server);
+let InitialCode=
+`
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World!";
+    return 0;
+}
+`;
 
-app.listen(process.env.PORT, ()=> console.log("Listening on Port", process.env.PORT));
+//Sync whatever is written already
+io.on('connection', (socket)=>{
+    console.log("Socket Connected", socket.id);
+    io.emit("code sync", InitialCode);
+    socket.on('code change', (msg) => {
+        console.log('message: ' + msg);
+        InitialCode=msg;
+        io.emit('code change', msg);
+
+      });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+})
+
+server.listen(process.env.PORT, ()=> console.log("Listening on Port", process.env.PORT));
 
 // app.listen(
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,47 @@
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import axios from "axios"
-import Editor from './Editor/Editor';
+// import Editor from './Editor/Editor';
+import CodeMirror from "@uiw/react-codemirror";
+import { initSocket } from './socket';
+
+
+
+
+
 function App() {
   const [code, setCode]= useState('');
   const [result, setResult]= useState('');
+  const socketRef= useRef(null);
+  useEffect(()=>{
+    const init = async ()=>{
+      socketRef.current= await initSocket();
+
+      // socketRef.current.emit('join', {
+      //   //data can be sent on Join
+      // });
+      socketRef.current.on('connect_error', (err)=>alert("Something Went Wrong"));
+      socketRef.current.on("connect_failed", (err)=>alert("Something went Wrong!"))
+      socketRef.current.on('code sync',(msg)=>{
+        console.log("Code, recieved: ", msg);
+        setCode(msg);
+      })
+      socketRef.current.on('code change',(CODE)=>{
+        console.log("Code, recieved: ", CODE);
+
+        setCode(CODE);
+      })
+      
+    }
+    init();
+  },[])
+
+  const onChange=  (value)=>{
+    console.log(value);
+     socketRef.current.emit('code change', value);
+    setCode(value);
+  }
   
   const handleSubmit = async()=>{
     const payload={
@@ -28,11 +64,21 @@ function App() {
   
   return (
     <div className="App">
-      <h1>Compiler</h1>
+      <h1 >Compiler</h1>
       <h2>Language Supported: c++</h2>
       
-      <Editor setCode={setCode} />
-      
+      {/* <Editor setCode={setCode} /> */}
+      <div style={{textAlign: "left"}}>
+        <CodeMirror
+          value={code}
+          height="200px"
+          theme="dark"
+        //   extensions={[javascript({ jsx: true })]}
+         
+          onChange={onChange}
+        />
+
+      </div>
 
       <br></br>
       <button onClick={handleSubmit}>Run</button>
